4742c7ab4b94791fb26e44fa4e8fa1273d199170
strogmann <nicholas.martin.galisteo@gmail.com>
2022-10-22 23:39:32 -0400

    primer commit (joder tu esto es mas facil que python)

---
+++ testeando1.rb
@@ -0,0 +1,23 @@
+amarillo = 12
+azul = 16
+rojo = "Ostia tu"
+isneverita = true
+
+marron = amarillo + azul
+
+puts marron + 2
+
+puts rojo
+
+puts "Como va la vida tronco?"
+puts "Baba Ganoush"
+
+puts "________"
+puts "| ---- |"
+puts "|  <>  |"
+puts "| ---- |"
+print "!______!"
+
+if isneverita
+    print "Manda huevos tio, vete de vuelta a puerto rico"
+end
